c78f9f24decadd24e8b99539801f2b0772d53177
devjeetroy98 <devjeetroy.dr@gmail.com>
2020-11-10 22:59:08 +0530

    Day 6

--- main/models.py
+++ main/models.py
@@ -1,5 +1,7 @@
 from main import db,login_manager
 from flask_login import UserMixin
+from flask_login import current_user
+from datetime import datetime,date
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -18,3 +20,18 @@ class User(db.Model, UserMixin):
 
     def __repl__(self):
         return f"User( '{self.id}' ,'{self.username}', '{self.email}' )"
+
+class Posts(db.Model):
+    post_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100),nullable=False)
+    content = db.Column(db.Text(),nullable=False)
+    author = db.Column(db.String(20),nullable=False)
+    time = db.Column(db.DateTime(),default=date.today(), onupdate=date.today())
+
+    def __init__(self, title,content,author):
+        self.title=title
+        self.content=content
+        self.author = author
+
+    def __repl__(self):
+        return f"Posts( '{self.title}' ,'{self.content}', '{self.time}' )"
--- main/routes.py
+++ main/routes.py
@@ -2,9 +2,9 @@ from flask.helpers import flash
 from flask_login.utils import login_user
 from main import app
 from flask import render_template, redirect, url_for, request
-from main.forms import RegistrationForm, LoginForm
+from main.forms import RegistrationForm, LoginForm, PostForm, validate_username
 from main import bcrypt, db
-from main.models import User
+from main.models import User,Posts
 from flask_login import current_user, logout_user, login_required
 
 @app.route('/')
@@ -18,7 +18,7 @@ def register_page():
     if request.method == 'GET':
         return render_template("register.html",form=form)
     else:
-        if form.validate_on_submit():
+        if form.validate_on_submit() and form.validate_username(form.username.data):
             hash_pass = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
             user = User(username=form.username.data, email=form.email.data, password=hash_pass)
             db.session.add(user)
@@ -62,6 +62,69 @@ def dashboard_page():
 def logout():
     if current_user.is_authenticated:
         logout_user()
+        flash('Successfully Logged Out!','success')
         return redirect(url_for('login_page'))
     else:
         return "<h2>Authentication Failed!</h2>"
+
+@app.route("/posts")
+@login_required
+def readblog():
+    if current_user.is_authenticated:
+        page = request.args.get('page', 1, type=int)
+        posts = Posts.query.order_by(Posts.post_id.desc()).paginate(page=page, per_page=5)
+        return render_template("readblog.html",posts=posts)
+    else:
+        return "<h2>Not Authorized!</h2>"
+
+@app.route("/post/new", methods=["GET","POST"])
+@login_required
+def writeblog():
+    form=PostForm()
+    if request.method == "GET":
+        if current_user.is_authenticated:
+            return render_template("postcontent.html", form=form)
+        else:
+            return "<h2>Not Authorized!</h2>"
+    else:
+        if form.validate_on_submit():
+            post = Posts(title=form.title.data, content=form.content.data, author=current_user.username)
+            db.session.add(post)
+            db.session.commit()
+            flash('Posted Successfully!','success')
+            return redirect(url_for('readblog'))
+        else:
+            return render_template("postcontent.html", form=form)
+
+@app.route("/posts/delete/<int:post_id>", methods=["POST"])
+@login_required
+def delete_blog(post_id):
+    if request.method == "POST":
+        if current_user.is_authenticated:
+            post = Posts.query.filter_by(post_id=post_id).first()
+            if post.author == current_user.username:
+                db.session.delete(post)
+                db.session.commit()
+                flash('Post deleted!','success')
+                return redirect(url_for('readblog'))
+        else:
+            return "<h2>Not Authorized!</h2>"
+
+@app.route("/posts/update/<int:post_id>", methods=["GET","POST"])
+@login_required
+def update_blog(post_id):
+    form=PostForm()
+    if request.method == "POST":
+        if current_user.is_authenticated:
+            post = Posts.query.filter_by(post_id=post_id).first()
+            if form.validate_on_submit():
+                post.title = form.title.data
+                post.content = form.content.data
+                db.session.commit()
+                flash('Updated Successfully!','success')
+                return redirect(url_for('readblog'))
+            form.title.data = post.title
+            form.content.data = post.content
+            return render_template("postcontent.html", form=form)
+        else:
+            return "<h2>Not Authorized!</h2>"
