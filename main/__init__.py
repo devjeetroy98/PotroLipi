31229a566982e412bfa6418573a4f391e78ef4a4
devjeetroy98 <devjeetroy.dr@gmail.com>
2020-11-13 22:31:12 +0530

    Blueprint modular pattern applied

--- main/__init__.py
+++ main/__init__.py
@@ -3,20 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-import os
+from main.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY']='2dc16a17401388067c61dafa40a5c72e2836844b0b9efbb367a7'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///auth.db'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-mail = Mail(app)
+mail = Mail()
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
 
-from main import routes
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    mail.init_app(app)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    from main.users.routes import users
+    from main.posts.routes import posts
+    from main.others.routes import others
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(others)
+
+    return app
--- main/users/forms.py
+++ main/users/forms.py
@@ -1,6 +1,6 @@
 from flask.app import Flask
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from main.models import User
 from wtforms.validators import ValidationError
@@ -30,16 +30,6 @@ class LoginForm(FlaskForm):
     password= PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Sign In!")
 
-class PostForm(FlaskForm):
-    title= StringField('Title', validators=[DataRequired()])
-    content= TextAreaField("Content", validators=[DataRequired()])
-    submit = SubmitField("Post!")
-
-
-def validate_username(self, username):
-    user = User.query.filter_by(username=username).first()
-    if user:
-        raise ValidationError('User already exist!')
 
 class  Reset_req_form(FlaskForm):
     email= StringField('E-Mail', validators=[DataRequired(), Email()])
