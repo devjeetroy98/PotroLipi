3621061e91023e8c314b0dd2a3e7a28533dbc0d6
devjeetroy98 <devjeetroy.dr@gmail.com>
2020-11-14 18:21:50 +0530

    Flask Blog Ready!

--- main/__init__.py
+++ main/__init__.py
@@ -25,9 +25,11 @@ def create_app(config_class=Config):
     from main.users.routes import users
     from main.posts.routes import posts
     from main.others.routes import others
+    from main.errors.handlers import errors
 
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(others)
+    app.register_blueprint(errors)
 
     return app
